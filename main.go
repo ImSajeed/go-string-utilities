3568bfdf67190fce95fac73d79001ccbe9eaf2af
Sajeed <sk.sajeed9@gmail.com>
2022-03-11 16:58:44 +0530

    adding storyStats function

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	testValidity("23-ab-48-caba-56-haha")
 	fmt.Println(averageNumber("23-ab-48-caba-56-haha"))
 	fmt.Println(wholeStory("23-ab-48-caba-56-haha"))
+	fmt.Println(storyStats("23-ab-48-caba-56-haha"))
 }
 
 /*
@@ -57,4 +58,58 @@ func wholeStory(inputStr string) string {
 		}
 	}
 	return strings.TrimSuffix(story, " ")
+}
+
+/*
+Estimated Time: 10 mins
+Used Time: 15 mins
+*/
+func storyStats(inputStr string) (int, int, int, []string) {
+	return findShortestWordLength(inputStr), findLongestWordLength(inputStr), averageWordLength(inputStr), wordsWithAverageWordLength(inputStr)
+}
+
+func findShortestWordLength(s string) (c int) {
+	arr := wholeStory(s)
+	words := strings.Split(arr, " ")
+	for _, word := range words {
+		if c == 0 || len(word) < c {
+			c = len(word)
+		}
+	}
+	return
+}
+
+func findLongestWordLength(s string) (c int) {
+	arr := wholeStory(s)
+	words := strings.Split(arr, " ")
+	for _, word := range words {
+		if c == 0 || len(word) > c {
+			c = len(word)
+		}
+	}
+	return
+}
+
+func averageWordLength(s string) (c int) {
+	totalWordLength := 0
+	length := 0
+	arr := wholeStory(s)
+	words := strings.Split(arr, " ")
+	for _, word := range words {
+		totalWordLength += len(word)
+		length += 1
+	}
+	return totalWordLength / length
+}
+
+func wordsWithAverageWordLength(s string) (list []string) {
+	arr := wholeStory(s)
+	avgWordLen := averageWordLength(s)
+	words := strings.Split(arr, " ")
+	for _, word := range words {
+		if len(word) == avgWordLen {
+			list = append(list, word)
+		}
+	}
+	return
 }
